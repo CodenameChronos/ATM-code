96895f04c51e531a1a755166b896b78622bbe849
Miguel Enrique Peraza Ramos <143283558+CodenameChronos@users.noreply.github.com>
2023-09-03 17:57:47 -0600

    Ultimate version?
    New "Create account feature". Linked to a csv file to read and write users and passwords in a new interface.

---
+++ ATM_2_1_2/ATM_2_1_2/src/atm_2/ATM_2.java
@@ -0,0 +1,65 @@
+package atm_2;
+import javax.swing.JOptionPane;
+public class ATM_2 {
+    
+    private Integer saldo = 0;
+    //private Integer accion = 0;
+    private final Saldo_compartido fondos = new Saldo_compartido();
+
+    public static void main(String[] args) {
+        Saldo_compartido fondos = new Saldo_compartido();
+        Login llamar = new Login();
+        llamar.setVisible(true);
+    }
+    
+     public void core(Login userEntryText, String nombreDeUsuario) {
+        saldo();
+        fondos.setSaldo(saldo);
+        Option_frame call = new Option_frame(fondos, this, nombreDeUsuario);
+        call.setVisible(true);
+        Option_frame.callto = this;
+        /*opcion();
+        if (accion != 2) {
+            ingresar();
+        } else {
+            retiro();
+        }
+        Login llamar = new Login();
+        llamar.setVisible(false);*/
+    }
+    
+        public void retiro(Saldo_compartido fondos){
+        int retirar;
+        do{
+        retirar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad a retirar"));
+        } while(retirar<0);
+        if (saldo>=retirar){
+            JOptionPane.showMessageDialog(null, "Se han retirado $"+retirar);
+            saldo=saldo-retirar;
+        } else{
+            JOptionPane.showMessageDialog(null, "Su saldo es insuficiente para esta transaccion");
+        }
+        fondos.setSaldo(saldo);
+    }
+    
+    public void saldo(){
+        do{
+            saldo = Integer.valueOf(JOptionPane.showInputDialog("Ingrese su saldo inicial"));
+        } while (saldo<=0);
+    }
+    
+    public void ingresar(Saldo_compartido fondos){
+        int abono;
+        do{
+        abono = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea abonar"));
+        } while (abono<=0);
+        JOptionPane.showMessageDialog(null, "Se han abonado $"+abono);
+        saldo=saldo+abono;
+        fondos.setSaldo(fondos.getSaldo() + abono);
+    }
+    
+    void setVisible() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ ATM_2_1_2/ATM_2_1_2/src/atm_2/Saldo_compartido.java
@@ -0,0 +1,17 @@
+package atm_2;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Saldo_compartido {
+    private int saldo;
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+}
